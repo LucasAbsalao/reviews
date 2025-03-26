6fc2901f7de39405fbc1e078535b7bb3c5bec407
Lucas Silveira Absalão <lsa4@cinsoftex.intranet>
2025-03-26 00:31:31 -0300

    fix: search bar for movies

--- app/components/showMovies/showMovies.jsx
+++ app/components/showMovies/showMovies.jsx
@@ -5,43 +5,57 @@ import ImageCard from './imageCard/imageCard'
 
 import { useEffect, useState } from "react";
 
-export default function FilterReviews() {
-    const [movies, setMovies] = useState(null)
+export default function ShowMovies({searchMovie}) {
+    const [removedMovie, setRemovedMovie] = useState(null)
     const [movieTitleCover, setMovieTitleCover] = useState([])
 
     useEffect(() => {
-            async function getMovies (){
-                const response = await fetch("http://localhost:5001/movies/", {
-                    method: "GET",
-                    headers: {
-                    "Content-Type": "application/json"
-                    },
-                });
-    
-                if (!response.ok) {
-                    throw new Error(`Erro na requisição: ${response.status}`);
-                }
-    
-                const movies = await response.json();
-    
-                const orderedMovies = movies.sort((a, b) => a.name.localeCompare(b.name));
-                const listImageTitle = orderedMovies.map(movie => ({
-                    name: movie.name, 
-                    coverURL: movie.cover.imageURL
-                }));
-                setMovieTitleCover(listImageTitle)
+        async function getMovies (){
+            const response = await fetch("http://localhost:5001/movies/", {
+                method: "GET",
+                headers: {
+                "Content-Type": "application/json"
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
             }
-            getMovies()
-        },[])
+
+            const movies = await response.json();
+
+            const orderedMovies = movies.sort((a, b) => a.name.localeCompare(b.name));
+            const listImageTitle = orderedMovies.map(movie => ({
+                name: movie.name, 
+                coverURL: movie.cover.imageURL
+            }));
+            setMovieTitleCover(listImageTitle)
+        }
+        getMovies()
+    },[])
+
+        // useEffect(() => {
+        //     function findByName(data, searchName) {
+        //         return data.filter(item => item.name.toLowerCase() !== searchName.toLowerCase());
+        //     }
+
+        //     findByName(movieTitleCover, removedMovie)
+        //     setRemovedMovie(null)
+        // },[removedMovie])
 
     return (
         <div id = 'container'>
             <div id='grid-container'>
-            {movieTitleCover && movieTitleCover.map((item, index) => (
-                <div key={index} id='grid-item'>
-                    <ImageCard imageURL={item.coverURL} title={item.name}/>
-                </div>
-            ))}
+                { searchMovie ? (
+                    <div key={index} id='grid-item'>
+                        <ImageCard imageURL={searchMovie.coverURL} title={searchMovie.name} setRemovedMovie={setRemovedMovie}/>
+                    </div>
+                ) :
+                (movieTitleCover && movieTitleCover.map((item, index) => (
+                    <div key={index} id='grid-item'>
+                        <ImageCard imageURL={item.coverURL} title={item.name} setRemovedMovie={setRemovedMovie}/>
+                    </div>
+                )))}
             </div>
         </div>
     );
--- app/pages/movies/page.js
+++ app/pages/movies/page.js
@@ -1,26 +1,83 @@
 'use client';
 
-
+import {useState, useEffect} from 'react'
 import {useRouter} from 'next/navigation'
 import ShowMovies from '../../components/showMovies/showMovies'
 
 import './style.css'
 
 export default function Home() {
+    const [filteredTitle, setFilteredTitle] = useState('')
+    const [filteredMovies, setFilteredMovies] = useState('')
+    const [buttonPressed, setButtonPressed] = useState(false)
     const router = useRouter()
 
     const handleClickCadastrar = () => {
         router.push('/pages/addMovie'); // Navega para a página de cadastro
     };
+
+    const handleSearchChange = (event) => {
+        setFilteredTitle(event.target.value);  // Atualiza o termo de busca
+        console.log(filteredTitle)
+    };
+
+    const handleButton = () => {
+        setButtonPressed(true)
+    }
+
+    useEffect(() => {
+        async function findByName (){
+            const requestBody = {}
+
+            if(filteredTitle) {
+                requestBody.name = filteredTitle
+                console.log(requestBody)
+                const response = await fetch("http://localhost:5001/movies/get", {
+                    method: "POST",
+                    headers: {
+                    "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(requestBody)
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Erro na requisição: ${response.status}`);
+                }
+
+                const movie = await response.json();
+                
+                const imageMovieTitle = {
+                    name: movie.movie.name, 
+                    coverURL: movie.movie.cover.imageURL
+                };
+                setFilteredMovies(imageMovieTitle)
+                setButtonPressed(false)
+                console.log(filteredMovies)
+            }
+        }
+        if (filteredTitle) {
+            findByName();
+        }
+        findByName()
+        
+    },[filteredTitle])
+
     return (
         <div id='home'>
             <div id="header-bar">
                 <div id="searchBar">
-                    <input type="text" placeholder="Buscar filme..." id="searchInput" />
-                    <button id="searchButton">🔍</button>
+                    <input type="text" placeholder="Buscar filme..." id="searchInput" value = {filteredTitle} onChange={handleSearchChange}/>
+                    <button id="lupa" onClick={handleButton}>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z"
+                                fill="#F9A826"
+                            />
+                        </svg>
+                    </button>
                 </div>
             </div>
-            <ShowMovies/>
+            <ShowMovies filteredMovies = {filteredMovies}/>
             <button id='cadastrar' onClick={handleClickCadastrar}>
                 Cadastrar
             </button>
